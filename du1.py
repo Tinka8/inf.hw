c8bc092aeb3279bb5416c6493eed0c4ee9a7f18e
Tinka8 <lenmatka818@gmail.com>
2021-02-17 22:21:58 +0100

    edit

--- du1.py
+++ du1.py
@@ -8,7 +8,7 @@ while value < 10:
     number = int(input("write a number: "))
     numbers.append(number)
 
-# if number is divisible by 3 with 0 rest then enlarge value by 1 
+# if number is divisible by 3 with 0 rest then increment value by 1 
     if number % 3 == 0:
         value += 1
        
